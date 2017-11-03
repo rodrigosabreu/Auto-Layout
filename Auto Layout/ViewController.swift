14a35fea7b94de276c8db069e83ceffb4e1f3227
Rodrigo Abreu <rodrigoabreu@rodrigos-macbook-pro.local>
2017-11-03 20:29:39 -0200

    Initial Commit

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by Rodrigo Abreu on 31/10/17.
+//  Copyright © 2017 Rodrigo Abreu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
